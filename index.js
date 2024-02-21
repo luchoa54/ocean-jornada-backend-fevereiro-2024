a8003f2eeb2a21deafa9d7aeb6e18fd42a9feb0e
Luciano Uchoa <103795412+luchoa54@users.noreply.github.com>
2024-02-21 15:14:03 +0000

    add delete function

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json())
 
- 
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
 app.get('/', function (req, res) {
 
   res.send('Hello World!')
 
 })
-
-const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
 
 app.get('/oi', function (req, res) {
 
@@ -32,5 +32,22 @@ app.get('/item/:id', function (req, res) {
     res.send(lista[id])
   }
 })
+
+app.post('/item', function (req, res) {
+  const body = req.body.nome
+  lista.push(body)
+  res.send("Item adicionado com sucesso!")
+}) 
+
+app.delete('/item', function (req, res) {
+  const body = req.body.nome
+
+  if(lista.includes(body)) {
+    lista.pop(body)
+    res.send("Item removido com sucesso!")
+  }else {
+    res.send("Item não está na lista!")
+  }
+}) 
 
 app.listen(3000)
