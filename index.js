ecc0cf6ec2d0c8bcecdf5c263e0ef5096323fea2
Luciano Uchoa <103795412+luchoa54@users.noreply.github.com>
2024-02-21 14:38:56 +0000

    add get item by index

--- index.js
+++ index.js
@@ -9,10 +9,28 @@ app.get('/', function (req, res) {
 
 })
 
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
 app.get('/oi', function (req, res) {
 
   res.send('Olá mundo')
 
+})
+
+app.get('/item', function (req, res) {
+
+  res.send(lista)
+
+})
+
+app.get('/item/:id', function (req, res) {
+  const id = req.params.id
+
+  if (id >= 3) {
+    res.send("Inválido")
+  }else {
+    res.send(lista[id])
+  }
 })
 
 app.listen(3000)
